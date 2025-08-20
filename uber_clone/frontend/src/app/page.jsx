3ec354972e04ebbaad7c199c12cdea19fdc06427
Pushkar Verma <pushkarverma2505@gmail.com>
2025-08-20 14:19:52 +0530

    Update page.jsx

--- uber_clone/frontend/src/app/page.jsx
+++ uber_clone/frontend/src/app/page.jsx
@@ -7,7 +7,7 @@ const main = () => {
     <div>
         <div className='w-full h-screen flex flex-col justify-between bg-black'>
               <div className=' w-full h-full pt-10 bg-cover bg-no-repeatr bg-[url(https://images.unsplash.com/photo-1704476944918-c1258561ebb9?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDU4fHx8ZW58MHx8fHx8)]' >
-                 <h1 className='flex text-white text-4xl justify-center items-center'>Cab X</h1>
+                 <h1 className='flex text-white text-4xl justify-center items-center'>Car X</h1>
                </div>
 
           <div className='h-1/4 pt-3 md:flex flex-col items-center  text-white bg-black px-5'>
